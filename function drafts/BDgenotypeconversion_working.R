00c7be2393ac25973c6277a72d73ae1ab889de1c
liviatran <tran.livia@berkeley.edu>
2018-09-06 21:37:17 -0700

    newest changes to ID CWD alleles for multi-loci data

---
+++ function drafts/BDgenotypeconversion_working.R
@@ -0,0 +1,29 @@
+BDgenotypeconversion<-function(gfeDirpath, genotypedata, allelefiles, cwdfiles){
+  if (is.data.frame(genotypedata)) {bigdawghladata <- genotypedata} else 
+  {switch(tolower(substr(genotypedata,nchar(genotypedata)-2,nchar(genotypedata))),
+          "csv" = bigdawghladata <- read.csv(genotypedata, stringsAsFactors = F, header=T, check.names = F), 
+          "txt" = bigdawghladata <- read.table(genotypedata, stringsAsFactors = F, header=T, sep="\t", check.names = FALSE), 
+          "tsv" = bigdawghladata <- read.table(genotypedata, stringsAsFactors = F, header=T,sep="\t",check.names = FALSE), 
+          print("Error: Unrecognized filename suffix."))}
+  hlamerged<-CWDrestriction(allelefiles, gfeDirpath)
+  hlafields<-strsplit(t(as.data.frame(matrix(unlist(strsplit(hlamerged[,1],"\\*")), nrow=nrow(hlamerged), byrow=T), stringsAsFactors=FALSE)) [2,],":")
+  hlalist<-list()            
+  for (i in 3:ncol(bigdawghladata)){
+    hlalist[[i]]<-strsplit(bigdawghladata[,i],":")} 
+  for(i in 3:ncol(bigdawghladata)){
+    bigdawghladata[i]<-paste(sapply(hlalist[[i]], "[", 1), sapply(hlalist[[i]], "[", 2), sep=":")
+    bigdawghladata[,i]<-paste(colnames(bigdawghladata[i]),bigdawghladata[,i],sep="*")}
+  for (i in 3:ncol(bigdawghladata)){
+    bigdawghladata[i]<-ifelse(hlamerged$CWD[match(bigdawghladata[,i], paste(gsub(".*[HLA-]([^*]+)[*].*", "\\1", hlamerged$allelename), paste(sapply(hlafields, "[", 1), sapply(hlafields, "[", 2), sep=":"), sep="*"), paste(gsub(".*[HLA-]([^*]+)[*].*", "\\1", hlamerged$allelename), paste(sapply(hlafields, "[", 1), sapply(hlafields, "[", 2), sep=":"), sep="*"))]=="CWD", 
+                              hlamerged$gfe[match(bigdawghladata[,i], paste(gsub(".*[HLA-]([^*]+)[*].*", "\\1", hlamerged$allelename), paste(sapply(hlafields, "[", 1), sapply(hlafields, "[", 2), sep=":"), sep="*"), paste(gsub(".*[HLA-]([^*]+)[*].*", "\\1", hlamerged$allelename), paste(sapply(hlafields, "[", 1), sapply(hlafields, "[", 2), sep=":"), sep="*")
+                              )], ifelse(hlamerged$CWD[match(paste(gsub(".*[HLA-]([^*]+)[*].*", "\\1", hlamerged$allelename), paste(sapply(hlafields, "[", 1), sapply(hlafields, "[", 2), sep=":"), sep="*"), paste(gsub(".*[HLA-]([^*]+)[*].*", "\\1", hlamerged$allelename), paste(sapply(hlafields, "[", 1), sapply(hlafields, "[", 2), sep=":"), sep="*"))]=="NON-CWD", 
+                                         hlamerged$gfe[match(paste(gsub(".*[HLA-]([^*]+)[*].*", "\\1",  hlamerged$allelename), paste(sapply(hlafields, "[", 1), sapply(hlafields, "[", 2), sep=":"), sep="*"), paste(gsub(".*[HLA-]([^*]+)[*].*", "\\1", hlamerged$allelename), paste(sapply(hlafields, "[", 1), sapply(hlafields, "[", 2), sep=":"), sep="*")
+                                         )], ""))} 
+  
+     return(bigdawghladata)}
+
+
+###samplebdHLA.csv is a csv version of the HLA_data set in BIGDAWG
+BDgenotypeconversion("/Users/liviatran/Desktop/ltmasterscoding/HLA", "samplebdHLA.csv", "Allelelist.3310.txt")
+
+warnings()
