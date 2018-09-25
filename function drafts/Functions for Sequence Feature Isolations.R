0bdc969df6ccd89e6900a8d240a26f59baf85033
liviatran <tran.livia@berkeley.edu>
2018-09-25 13:45:55 -0700

    with analysis of stratified alleles

--- function drafts/Functions for Sequence Feature Isolations.R
+++ function drafts/Functions for Sequence Feature Isolations.R
@@ -1,7 +1,7 @@
 ###Sequence Feature Isolation 
 ##By: Livia Tran 
-#V 1.6
-#September 24, 2018
+#V 1.7
+#September 25, 2018
 
 ##This script aims to isolate pre-determined feature sequences for a given allele (in this case, HLA)
 #referenced by a user-made atlas, which is guided by a user-made framework containing information on
@@ -195,28 +195,87 @@ return(mergedlist)
 #mapname
 #if the user specifies "all" into mapname, all feature sequences and feature groups present in the atlas
 #are returned for a given BIGDAWG formatted file
+#if the user does not specify anything for mapname, the default will be "all"
 
-dataConvert<-function(mergedcustomdata=custom_mergeddata, mapname, BIGDAWGgenotypedata, alleleListfiles, info=F){
+dataConvert<-function(mergedcustomdata=custom_mergeddata, mapname="all", BIGDAWGgenotypedata, alleleListfiles, info=F){
   #requires BIGDAWG package to run loci, haplotype, and HWE tests on data
   require(BIGDAWG)
   if(info==TRUE){cat(paste("The following ‘maps’ are available in the atlas:",paste(colnames(atlas)[2:ncol(atlas)],collapse=" "),sep="\n"))}
   if(info==FALSE){
-  if(any(mapname==colnames(atlas[,2:length(atlas)]))){
-  convertedlist<-list()
-  #converts BIGDAWG formatted data based on feature desired, followed by immediate analysis for all
-  #three available tests
-  BIGDAWG(BDgenotypeconversion(BIGDAWGgenotypedata, alleleListfiles, custom_mergeddata[[mapname]]), HLA=F, Run.Tests = c("HWE", "H", "L"))}
-    if(any(mapname=="all")){
-    convertedlist<-sapply(colnames(atlas[,2:length(atlas)]),function(x) NULL)
+    if(any(mapname%in%colnames(atlas[,2:length(atlas)]))==TRUE){ 
+      convertedlist<-list()
+      #converts BIGDAWG formatted data based on feature desired, followed by immediate analysis for all
+      #three available tests
+      BIGDAWG(BDgenotypeconversion(BIGDAWGgenotypedata, alleleListfiles, custom_mergeddata[[mapname]]), HLA=F, Run.Tests = c("HWE", "H", "L"))
+    return(BDgenotypeconversion(BIGDAWGgenotypedata, alleleListfiles, custom_mergeddata[[mapname]]))}
+    if(any(isTRUE(mapname=="all")==TRUE)){
+      convertedlist<-sapply(colnames(atlas[,2:length(atlas)]),function(x) NULL)
       #for loop for converting BIGDAWG formatted data into its GFE components based on all maps,
-    #followed by immediate analysis for all tests in BIGDAWG 
+      #followed by immediate analysis for all tests in BIGDAWG 
       for(i in 1:length(custom_mergeddata)){
         convertedlist[[i]]<-BDgenotypeconversion(BIGDAWGgenotypedata, alleleListfiles, custom_mergeddata[[i]])}
+      View(convertedlist)      
       BIGDAWG(convertedlist[[i]], HLA=F, Run.Tests = c("L", "HWE", "H"))}}}
 
 
 ############END SCRIPT for dataConvert()
 
+#### BDStrat -- simple allele stratification of BIGDAWG formatted datasets  
+####            Steven J. Mack September 13 - 18, 2018 v0.1
+
+###             BDStrat() accepts a BIGDAWG formatted genotype data file and generates a list containing a pair of subset 
+###             dataframes (named locus*allelles-positive and locus*alleles-negative) in which one dataframe contains all 
+###             subjects with a specified allele, and the second dataframe contains all subjects without the specified 
+###             allele. These dataframes can then be passed to BIGDAWG for analyses stratified on the specified allele. 
+
+###             Current version (v0.1) is limited to stratifying on multiple alleles at a single locus
+###             Current version assumes the BIGDAWG dataset is tab-delimited
+
+# Examples 
+# stratified <- BDStrat("MS_EUR.txt","DRB1",c("15:01","11:04"))
+# stratified2 <- BDStrat(HLA_data,"DRB1",c("15:01:01:01","11:04:01"))
+
+BDStrat <- function(dataset,locus,alleles){
+  
+  #for(i in length(locus)){ ## hook for a future (more complicated) version that stratifies on multiple loci
+  locus <- c(locus,paste(locus,"1",sep="."))
+  #}
+  
+  # Since all loci are duplicated, check.names = TRUE generates "locus","Locus.1" name pairs for read files
+  # and make.names(x,unique=TRUE) does the same for data frames
+  if(!is.data.frame(dataset)) { 
+    dataset <- read.table(dataset,header = TRUE,sep="\t",check.names = TRUE)
+  } else { colnames(dataset) <- make.names(colnames(dataset),unique = TRUE)}
+  
+  # Everything is stored in the strataSet list 
+  stratSet <- data.frame(rep(NA,nrow(dataset)))
+  
+  # Identify the rows containing each target allele for each locus column
+  for(i in 1:length(alleles)){
+    stratSet <- cbind(stratSet,dataset[[locus[1]]]==alleles[i],dataset[[locus[2]]]==alleles[i])
+  }
+  
+  # Identify the rows containing any target allele
+  for(i in 1:nrow(stratSet)){
+    stratSet[i,1] <- any(unlist(stratSet[i,2:((2*length(alleles))+1)]))
+  }
+  
+  # Split the parent dataset into two stratified subsets
+  posStrat <- dataset[stratSet[,1]==TRUE,]
+  negStrat <- dataset[stratSet[,1]==FALSE,]
+  
+  # Add them as elements of the stratPair list, named with the selected or excluded alleles
+  stratPair <- list()
+  stratPair[[paste(locus[1],"*",paste(unlist(alleles),collapse=paste("+",locus,"*",sep="")),"-positive",sep="")]] <- posStrat
+  stratPair[[paste(locus[1],"*",paste(unlist(alleles),collapse=paste("+",locus,"*",sep="")),"-negative",sep="")]] <- negStrat
+  
+  # Strip suffixes from column names
+  colnames(stratPair[[1]]) <- gsub(".1","",colnames(stratPair[[1]]),fixed=TRUE)
+  colnames(stratPair[[2]]) <- gsub(".1","",colnames(stratPair[[2]]),fixed=TRUE)
+  
+  #return object
+  stratPair
+}
 
 
 ####EXAMPLES
@@ -238,6 +297,9 @@ custom_mergeddata<-customGFEgenerator("/Users/liviatran/Desktop/ltmasterscoding/
 #"/Users/liviatran/Desktop/ltmasterscoding/Allelelist.3310.txt" is a list of documented HLA alleles with
 #their allele IDs 
 #obtained from https://github.com/ANHIG/IMGTHLA/blob/Latest/Allelelist.3310.txt
-dataConvert(custom_mergeddata, "fiveUTR", HLA_data, "/Users/liviatran/Desktop/ltmasterscoding/Allelelist.3310.txt")
+converteddata<-dataConvert(custom_mergeddata, "fiveUTR", HLA_data, "/Users/liviatran/Desktop/ltmasterscoding/Allelelist.3310.txt")
 
 
+#runs BIGDAWG on negative list of stratified data
+#as positive list contains NA's only 
+BIGDAWG(BDStrat(converteddata,"DRB1",c("15:01:01:01","11:04:01"))[[2]], HLA=F, Run.Tests = c("HWE", "H", "L"))
