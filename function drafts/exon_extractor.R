7a5b807f92f7c0d3d8479cfc200b9c98c2eaf214
liviatran <tran.livia@berkeley.edu>
2018-10-19 19:04:14 -0700

    v 0.5

--- function drafts/exon_extractor.R
+++ function drafts/exon_extractor.R
@@ -1,158 +1,132 @@
 #exon_extractor
-#V 0.4
+#V 0.5 - 10/19/18
 #By: Livia Tran
 
+###NOTE: this script is in its early stages and is using only the A_prot.txt and B_prot.txt files 
+#for code execution
 
-###NOTE: this script is in its early stages and is using only the A_prot.txt file for code execution
-##areas where the locus is hard-coded will be fixed later on when the code is adapted for
-#universal usage for all HLA loci 
-#fetches "_prot.txt files from IMGTHLA for a specified HLA locus
-#downloads directly to working directory
-#additionally reads the .txt file in, storing protein information for each locus as an element
-#in a list named "alignment" 
-#strips white space
-#fill=T for blank rows
-loci="A"
+#requires data.table package
+require(data.table)
+
+#downloads appropriate loci_prot.txt files from ANHIG and reads them in
+#each individual locus will be an element alignment, an empty list created to
+#store protein sequences for each locus 
+#skips first 5 lines containing citation information
+#clips last three lines containing blank spaces and impertinent information
+#removes white space between peptide sequences for all loci
+#preserves white space between allele name and peptide sequence for strsplit later on
+#as.matrix dimensions for strsplit later on 
+loci=c("A", "B")
 alignment<-list()
 for(i in 1:length(loci)) {
-    locus <- loci[i]
-    filename <- paste(locus,"_prot.txt",sep="")
-    URL <- paste("https://raw.githubusercontent.com/ANHIG/IMGTHLA/Latest/alignments/",filename,sep="")
-    download.file(URL,destfile = filename, method="libcurl")
-    name <- paste(loci,"_prot.txt",sep="")
-    alignment[[name[i]]]<- read.table(name[i],fill=T,header=F,stringsAsFactors=F,strip.white=T,colClasses="character", sep="\t")
-  }
+  alignment[[loci[i]]] <- fread(paste("https://raw.githubusercontent.com/ANHIG/IMGTHLA/3330/alignments/",paste(loci[i],"_prot.txt",sep=""),sep=""), fill=T, sep="\t", skip=7, header=F,strip.white=T,colClasses="character")
+  alignment[[loci[i]]] <- as.matrix(head(alignment[[i]],-3)) 
+  alignment[[loci[i]]] <-as.matrix(paste(substr(alignment[[i]],1,regexpr(" ",text = alignment[[i]],fixed = TRUE)), gsub(" ","",substr(alignment[[i]],regexpr(" ",text = alignment[[i]],fixed = TRUE),nchar(alignment[[i]]))),sep = ""))}
 
-#removes footer at the end of the text file
-alignment <- as.matrix(alignment[[1]][-nrow(alignment[[1]]),]) 
 
-#removes white space between peptide sequences for all elements in alignment 
-for(i in 1:nrow(alignment)){
-  alignment[i]<-paste(substr(alignment[i],1,regexpr(" ",text = alignment[i],fixed = TRUE)[1]),gsub(" ","",substr(alignment[i],regexpr(" ",text = alignment[i],fixed = TRUE)[1],nchar(alignment[i]))),sep="")}
+#creates empty variables for the next for loop to input start/end values for each HLA locus
+start<-list()
+end<-list()
 
-#splits column 1 of alignment at the white space between
-#allele and amino acid sequence to delineate allele name from AA sequences
-alignment <- strsplit(alignment[,1]," ")
-
-#binds the previously split list rowwise -- output is an allele name column and the AA sequences column
-alignment <- as.matrix(do.call(rbind,alignment))
-
+#splits allele names from peptide sequences for each locus
+#binds  previously split list rowwise -- output is an allele name column and the AA sequences column
+#for each locus
 #finds columns where the allele name is equal to the AA sequences - occurs when expression is affected
 #due to some mutation
-alignment[which(alignment[,1]==alignment[,2]),2] <- ""
-
 #defines start as where "Prot" is present
 #since Prot is at the start of the next set of AA sequences
-start<-which(alignment[,1]=="Prot")
-
 #defines end as the second element in start -1 row to signify the end of that segment
 #includes the final row of the matrix
-end <- c(start[2:length(start)]-1,nrow(alignment))
+for(i in 1:length(alignment)){
+alignment[[loci[i]]]  <- strsplit(alignment[[i]]," ", fixed=T)
+alignment[[loci[i]]]  <- as.matrix(do.call(rbind,alignment[[i]]))
+alignment[[i]][which(alignment[[i]][,1]==alignment[[i]][,2]),2] <- ""
+start[[loci[i]]]<-as.numeric(which(alignment[[i]][,1]=="Prot"))
+end[[loci[i]]] <- as.numeric(c(start[[i]][2:length(start[[i]])]-1,nrow(alignment[[i]])))
+}
 
-#for loop for parsing out starts and ends for each set of AA sequences
-#output is a matrix with allele names and each subsequent set of AA sequences 
-AA_segments<-NULL
-for(i in 1:length(start)){
-  AA_segments <- cbind(AA_segments,alignment[start[i]:end[i],])}
+#for loop for parsing out starts and ends for each set of amino acid sequences
+#output is a matrix with allele names and each subsequent set of amino acid sequences
+AA_segments <- sapply(names(alignment),function(x) NULL)
 
-#removes header, which contains AA position and "Prot"
-AA_segments <- AA_segments[-c(1,2),]
+for(i in 1:length(alignment)){
+  for(j in 1:length(start[[i]])){
+    AA_segments[[i]]<-cbind(AA_segments[[i]], alignment[[i]][start[[i]][j]:end[[i]][j],])}}
 
+#for loop for removing headers, which contains AA position and "Prot"
 #designates columns to be combined as every other so allele names are not included
 #in pasting all the amino acid sequences together 
-cols<-seq(0, ncol(AA_segments), by=2)
-AA_segments<-cbind(AA_segments[,1], apply(AA_segments[,cols], 1 ,paste, collapse = ""))
+for(i in 1:length(AA_segments)){
+AA_segments[[i]] <- AA_segments[[i]][-c(1,2),]
+cols<-seq(0, ncol(AA_segments[[i]]), by=2)
+AA_segments[[i]]<-cbind(AA_segments[[i]][,1], apply(AA_segments[[i]][,cols], 1 ,paste, collapse = ""))}
 
-#string split to extract locus in the allele name
-#assigns to new variable
-AA_aligned <- do.call(rbind,strsplit(AA_segments[,1],"[*]"))
-
+#for loop for string split to extract locus in the allele name
+#assigns to new variable "AA_aligned"
 #trims allele name down to two fields -- based on GetField function from BIGDAWG general functions
 #combines previous AA_segments matrix with AA_aligned matrix, for a total of 5 columns
+#AA_aligned extracts locus information to input to AA_segments 
 #assigns column names based on what each column contains 
-AA_segments <- cbind(cbind(AA_aligned,apply(AA_aligned,MARGIN=c(1,2),FUN=GetField,Res=2)[,2]),AA_segments)
-colnames(AA_segments) <- c("locus","full_allele","trimmed_allele","allele_name","AAsequence")
+AA_aligned<-sapply(names(alignment),function(x) NULL)
 
-#commented out for further development when testing multiple loci
-##for later usage when testing out on multiple alleles -- ensures locus specific rows
-#AA_segments <- AA_segments[which(AA_segments[,'locus']=="A"),]
+for(i in 1:length(AA_segments)){
+AA_aligned[[i]] <- do.call(rbind,strsplit(AA_segments[[i]][,1],"[*]"))
+AA_segments[[i]] <- cbind(cbind(AA_aligned[[i]],apply(AA_aligned[[i]],MARGIN=c(1,2),FUN=GetField,Res=2)[,2]),AA_segments[[i]])
+colnames(AA_segments[[i]]) <- c("locus","full_allele","trimmed_allele","allele_name","AAsequence")}
 
-#creates a reference table data frame with the first row of AA_segments, which contains 
-#reference information
-#binds with a new column row named ref.start, which contains information for where 
-#an exon starts 
-reftable<-as.data.frame(cbind(rbind(AA_segments[1,]), ref.start=1), stringsAsFactors = FALSE)
+#sets refexon to a reference peptide for each HLA locus based on the reference sequences in
+#AA_segments 
+refexon<- sapply(names(alignment),function(x) NULL)
+for(i in 1:length(AA_segments)){
+refexon[[i]] <- rbind(AA_segments[[i]][1,])[which(rbind(AA_segments[[i]][1,])[,"locus"]==loci[[i]]),'AAsequence']}
 
-####in this case, the locus is hard-coded to A, as this code is still in development before testing
-#it out on all HLA loci 
-#sets RefExon to a Reference Peptide, depending on locus desired 
-RefExon <- reftable[which(reftable[,"locus"]=="A"),'AAsequence']
+#sets start as 1 for each loci
+exon_start<-sapply(names(alignment),function(x) NULL)
 
-#sets RefStart to a locus specific start position 
-RefStart <- as.numeric(reftable[which(reftable[,'locus']=="A"),'ref.start'])
-
-#sets RefAllele to a locus specific reference allele 
-RefAllele <- reftable[which(reftable[,'locus']=="A"),'allele_name']
-
-#designates start position in the full reference peptide sequence in AA_segments
-#by finding what position it starts at based on RefExon
-exon_start <- as.numeric(regexpr(RefExon,AA_segments[1,'AAsequence']))
-
-#designates end position in the full reference peptide sequence in AA_segments
-#by adding exon start to the number of characters in RefExon -1 
-exon_end <- (exon_start + nchar(RefExon))-1
-
-#subsets reference peptide sequences in AA_segments based on found exon start and end in previous lines
-AA_segments[,'AAsequence'] <- substr(AA_segments[,'AAsequence'],exon_start,exon_end)
+for(i in 1:length(AA_segments)){
+exon_start[[i]]<-1}
 
 #splits AA_sequence column at every amino acid, resulting in a list of split amino acids for each row
-pep_split <- sapply(AA_segments[,'AAsequence'],strsplit,split="*")
-
-#fills all allele rows with "-" until the length of the RefExon
-#rows with less characters are likely due some mutation 
-pep_split<- lapply(pep_split,function(x) c(x,rep("NA",nchar(RefExon)-length(x))))
-
 #binds pep_split together by element in its previous list form by row
 #nullifies row names 
-pep_split <- do.call(rbind,pep_split)
-rownames(pep_split) <- NULL
+#binds the first 4 columns of AA_segments with amino acid positions from pepsplit 
+pepsplit<-sapply(names(alignment),function(x) NULL)
 
-#binds the first 4 columns of AA_segments with amino acid positions from pep_split 
-AAsequences <- cbind(AA_segments[,1:4],pep_split)
-
-#renames each column
-#with each AA getting its own column name, depending on its position in the original peptide sequence
-colnames(AAsequences) <- c("locus","allele","trimmed_allele","allele_name",seq(RefStart,ncol(AAsequences[,5:ncol(AAsequences)])+RefStart-1))
+for(i in 1:length(AA_segments)){
+pepsplit[[i]] <- sapply(AA_segments[[i]][,"AAsequence"],strsplit,split="*")
+pepsplit[[i]]<- lapply(pepsplit[[i]],function(x) c(x,rep("NA",nchar(refexon[[i]])-length(x))))
+pepsplit[[i]] <- do.call(rbind,pepsplit[[i]])
+rownames(pepsplit[[i]]) <- NULL
+AA_segments[[i]] <- cbind(AA_segments[[i]][,1:4],pepsplit[[i]])
+colnames(AA_segments[[i]]) <- c("locus","allele","trimmed_allele","allele_name",seq(exon_start[[i]],ncol(AA_segments[[i]][,5:ncol(AA_segments[[i]])])+exon_start[[i]]-1))
+}
 
 #for loop for distributing the reference sequence from row 1
 #into all other rows, if the contain a "-"
 #amino acids with changes will not be impacted
-for(i in 5:ncol(AAsequences)) {
-  x <- AAsequences[,i]
-  x[which(x=="-")] <- x[1]
-  AAsequences[,i] <- x
-}
+for(i in 1:length(AA_segments)){
+  for(j in 5:ncol(AA_segments[[i]])) {
+    AA_segments[[i]][,j][which(AA_segments[[i]][,j]=="-")] <- AA_segments[[i]][,j][1]
+  }}
 
-#NOTE: locus is hard-coded here -- will be fixed later
-#adds in *00:00 alleles to account for alleleic absences 
+#adds in *00:00 alleles to account for alleleic absences for each locus previously defined by loci
+#at the beginning of the script 
 #turns matrix into a dataframe
-AAsequences <- as.data.frame(rbind(c("A","00:00:00:00","00:00",paste("A","*00:00:00:00",sep=""), rep(".",ncol(AAsequences)-4)),
-                                   AAsequences), stringsAsFactors = FALSE)
-               
+for(i in 1:length(AA_segments)){
+AA_segments[[i]] <- data.frame(rbind(c(loci[[i]],"00:00:00:00","00:00",paste(loci[[i]],"*00:00:00:00",sep=""), rep(".",ncol(AA_segments[[i]])-4)),
+AA_segments[[i]]), stringsAsFactors = FALSE, check.names = F)}
 
 #AA_atlas.R is used as a guide for determining start and stop positions for each exon for a given HLA locus
 load("AA_atlas.rda")
 
-#creates empty list
-exonlist<-list()
-
-#for loop for subsetting AA_matrix by matching exon start and end cells from AA_atlas
-#column names of AA_matrix, which are AA positions
+#for loop for subsetting AA_sequences by matching exon start and end cells from AA_atlas
+#column names of AA_sequences, which are AA positions
 #subsets relevant amino acids, inputting them into a list
 #binds previous columns with locus, allele, trimmed allele, and allele name information 
-for(i in 1:nrow(AA_atlas)){
-exonlist[[AA_atlas$exon[i]]]<-cbind(AAsequences[,1:4], AAsequences[,match(AA_atlas[i,2],colnames(AAsequences)):match(AA_atlas[i,3],colnames(AAsequences))])}
+exonlist<-sapply(names(alignment),function(x) NULL)
+for(i in 1:length(AA_segments)){
+  for(j in 1:nrow(AA_atlas[[i]]))
+exonlist[[i]][[j]]<-cbind(AA_segments[[i]][,1:4], AA_segments[[i]][match(AA_atlas[[i]][j,2],colnames(AA_segments[[i]])):match(AA_atlas[[i]][j,3],colnames(AA_segments[[i]]))])}
 
-
-View(exonlist[[1]])
+View(exonlist[[1]][[1]])
 
