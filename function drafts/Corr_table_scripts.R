6128d511a5e9f6ee100b8fb6e19005ebde2ba557
liviatran <tran.livia@berkeley.edu>
2018-11-03 22:50:36 -0700

    v 0.3

--- function drafts/Corr_table_scripts.R
+++ function drafts/Corr_table_scripts.R
@@ -1,22 +1,22 @@
 ###Correspondence table for HLA-A and HLA-C
 #By: Livia Tran
-#11/2/2018
-#v 0.2
+#11/3/2018
+#v 0.3
 
-loci="C"
+loci=c("A", "C")
 
 #requires stringr package 
 require(stringr)
 
 #creates empty variables based on "loci" to be later filled in by for loops 
-end<-corr_table<-downloaded_segments<-alignment_positions<-alignment_length<-alignment_start<-downloaded<-prot_extractions<-diff<-prot_start<-end_char<-refblock_number<-sapply(loci, function(x) NULL)
+cols<-w<-end<-corr_table<-downloaded_segments<-alignment_positions<-alignment_length<-alignment_start<-downloaded<-prot_extractions<-diff<-prot_start<-end_char<-refblock_number<-sapply(loci, function(x) NULL)
 
 
 for(i in 1:length(loci)) {
   
   #downloads relevant locus alignment file -- readLines allows for space preservation, which is important in
   #finding where the alignment sequence starts 
-  downloaded[[loci[i]]] <- readLines(paste("https://raw.githubusercontent.com/ANHIG/IMGTHLA/Latest/alignments/",paste(loci,"_prot.txt",sep=""),sep=""),-1,ok=TRUE,skipNul = FALSE)
+  downloaded[[loci[i]]] <- readLines(paste("https://raw.githubusercontent.com/ANHIG/IMGTHLA/Latest/alignments/",paste(loci[[i]],"_prot.txt",sep=""),sep=""),-1,ok=TRUE,skipNul = FALSE)
   
   #alters downloaded file by cutting out non-pertinent information 
   downloaded[[loci[i]]] <- head(downloaded[[i]],-3)
@@ -39,36 +39,38 @@ for(i in 1:length(loci)) {
   #counts number of characters in the very last allele to add onto the last Prot enumeration block
   #to obtain end length 
   end_char[[loci[i]]]<-nchar(sapply(strsplit(gsub(" ", "", sub(" ", "~", str_squish(tail(downloaded[[i]], 1)))), "~"), "[", 2))-1
-  
+
+
   #extracts rows with "Prot" and reference sequence position information 
   #extracts only relevant reference sequence positions
   #NOTE: the first row containing "Prot" contains two numbers -- -30 and 1 -- where only -30, is extracted,
   #as the actual sequence start will always be 1 
-  for (j in 1:length(prot_start)){
-    prot_extractions[[j]]<-downloaded[[i]][prot_start[[j]]]
-    prot_extractions[[j]]<-strsplit(prot_extractions[[j]], " ")
-    refblock_number[[j]]<-as.numeric(sapply(prot_extractions[[j]], "[", 2))
+  for (j in 1:length(prot_start[[i]])){
+    prot_extractions[[i]][j]<-strsplit(downloaded[[i]][prot_start[[i]][j]], " ")
     
-    #determines the alignment start by adding -30 to the difference between white spaces found above 
-    alignment_start[[loci[i]]]<-refblock_number[[j]][1]+diff[[i]]
-    
+  refblock_number[[i]][j]<-as.numeric(sapply(prot_extractions[[i]][j], "[", 2))}
+  
+
+  #determines the alignment start by adding -30 to the difference between white spaces found above 
+    alignment_start[[loci[i]]]<-refblock_number[[i]][1]+diff[[i]]
+
+
     #determines alignment length based on the last Prot enumeration + the number of characters in the last row 
-    alignment_length[[loci[i]]]<-as.numeric(tail(refblock_number[[j]], 1))+end_char[[i]]
-    
+    alignment_length[[loci[i]]]<-as.numeric(tail(refblock_number[[i]], 1))+end_char[[i]]
+
     #pastes alignment_start to alignment_length together in sequential order, with inDels accounted for 
     #captures output as "w"
-    if(loci[[i]]=="A"){
-    w<-capture.output(cat(alignment_start[[i]]:(as.numeric(refblock_number[[j]][4])-(alignment_start[[i]])), (as.numeric(refblock_number[[i]][4])-alignment_start[[i]]+1):alignment_length[[i]]))}
-    if(loci[[i]]=="C"){
-      w<-capture.output(cat(alignment_start[[i]]:(as.numeric(refblock_number[[j]][4])-(alignment_start[[i]])), paste("inDel", seq(1:6), sep=""), (as.numeric(refblock_number[[i]][4])-alignment_start[[i]]+1):alignment_length[[i]]))
-      
-    }
-    
+    if(loci[i]=="A"){
+    w$A<-capture.output(cat(alignment_start[[i]]:(as.numeric(refblock_number[[i]][4])-(alignment_start[[i]])), (as.numeric(refblock_number[[i]][4])-alignment_start[[i]]+1):alignment_length[[i]]))}
+    if(loci[i]=="C"){
+      if(loci[[i]]=="C"){
+    w$C<-capture.output(cat(alignment_start[[i]]:(as.numeric(refblock_number[[i]][4])-(alignment_start[[i]])), paste("inDel", seq(1:6), sep=""), (as.numeric(refblock_number[[i]][4])-alignment_start[[i]]+1):alignment_length[[i]]))}    }
+
     #splits string formed by cat for separate character variables
-    alignment_positions[[i]]<-as.character(unlist(strsplit(w, " ")))
+    alignment_positions[[i]]<-as.character(unlist(strsplit(w[[i]], " ")))
     
     #eliminates "0", as the alignment sequence from ANHIG does not contain 0
-    alignment_positions[[i]]<-alignment_positions[[i]][-which(alignment_positions[[i]] == 0)]}
+    alignment_positions[[i]]<-alignment_positions[[i]][-which(alignment_positions[[i]] == 0)]
   
   #closes all white space in the downloaded file, except for the white space separating the allele and peptide sequence
   downloaded[[loci[i]]] <-paste(substr(downloaded[[i]],1,regexpr(" ",text = downloaded[[i]],fixed = TRUE)), gsub(" ","",substr(downloaded[[i]],regexpr(" ",text = downloaded[[i]],fixed = TRUE),nchar(downloaded[[i]]))),sep = "")
@@ -98,19 +100,24 @@ for(i in 1:length(loci)) {
 
 for(i in 1:length(loci)){
   for(k in 1:length(prot_start[[i]])){
-    
     #subsets downloaded file by start and stop positions -- binds all subsets together, where the number of characters
     #in the reference sequence may be obtained to input as the end position for the correspondence table 
-    downloaded_segments[[i]]<-cbind(downloaded_segments[[i]], downloaded[[i]][prot_start[[i]][k]:end[[i]][k],])}
-  cols<-seq(0, ncol(downloaded_segments[[i]]), by=2)
-  downloaded_segments[[i]]<-cbind(downloaded_segments[[i]][,1], apply(downloaded_segments[[i]][,cols], 1 ,paste, collapse = ""))
-  rownames(downloaded_segments[[i]])=NULL
+  downloaded_segments[[i]]<-cbind(downloaded_segments[[i]], downloaded[[i]][,2][prot_start[[i]][k]:end[[i]][k]])}
+
+##stopped here
+  cols[[i]]<-seq(1, ncol(downloaded_segments[[i]]), by=1)
   
-  #creates a new matrix with the number of columns equal to the number of characters in the reference sequence 
-  corr_table[[i]]<-matrix(, nrow = 2, ncol = as.numeric(nchar(downloaded_segments[[i]][,2][3])))
-  
+
+downloaded_segments[[i]]<-apply(downloaded_segments[[i]][,cols[[i]]] , 1 , paste , collapse = "" )
+
+
+rownames(downloaded_segments[[i]])=NULL
+
+#creates a new matrix with the number of columns equal to the number of characters in the reference sequence 
+  corr_table[[i]]<-matrix(, nrow = 2, ncol = as.numeric(nchar(downloaded_segments[[i]][3])))
+
   #contains actual sequence information
-  corr_table[[i]][1,]<-(1:as.numeric(nchar(downloaded_segments[[i]][,2][3])))
+  corr_table[[i]][1,]<-(1:as.numeric(nchar(downloaded_segments[[i]][3])))
   
   #contains alignment sequence information 
   corr_table[[i]][2,]<-alignment_positions[[i]]
@@ -136,5 +143,3 @@ countSpaces <- function(x){
 }
 
 
-
-View(alignment_positions[[1]])
