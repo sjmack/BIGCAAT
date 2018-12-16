e41b94cf60ef153d606e68cfb9d5920a384c4bc1
liviatran <tran.livia@berkeley.edu>
2018-12-15 23:34:12 -0800

    DRB addition

--- function drafts/AA_atlas.R
+++ function drafts/AA_atlas.R
@@ -1,24 +1,26 @@
 #AA determination for exons
 #atlas for HLA-A, B, C, DPA1, DPB1, DQA1, DQB1, DRB1, DRB3/4/5
-#v 0.5 - 10/25/18
+#v 0.6 - 10/27/18
 #By:Livia Tran 
 
 AA_atlas<-list()
-loci<-c("A", "B", "C", "DPA1", "DPB1", "DQA1", "DQB1", "DRB1", "DRB3", "DRB4","DRB5")
+loci<-c("A", "B", "C", "DRB", "DPB1", "DQB1") 
 
-AA_atlas[[loci[1]]] <- data.frame(exon=paste("exon", seq(1:8)), stringsAsFactors = FALSE)
-AA_atlas[[loci[[1]]]]$start <-list(1, 25,115, 207, 299, 338, 349, 365)
-AA_atlas[[loci[[1]]]]$end<- list(24, 114, 206, 298, 337, 348, 364, 365)
+i=1:7
+AA_atlas[[loci[1]]] <- data.frame(exon=c(paste("exon", paste(i, i+1, sep=":"), sep="_")), stringsAsFactors = FALSE)
+AA_atlas[[loci[[1]]]]$bound <-list(1, 91, 183, 275, 314, 325, 341)
 
-AA_atlas[[loci[2]]] <- data.frame(exon=paste("exon", seq(1:7)), stringsAsFactors = FALSE)
-AA_atlas[[loci[[2]]]]$start<-list(1, 24, 114, 206, 299, 339, 249)
-AA_atlas[[loci[[2]]]]$end<-list(23, 113, 205, 298, 337, 348, 363)
-
-AA_atlas[[loci[3]]] <- data.frame(exon=paste("exon", seq(1:8)), stringsAsFactors = FALSE)
-AA_atlas[[loci[[3]]]]$start <-list(1, 25, 115, 206, 297, 337, 348, 364)
-AA_atlas[[loci[[3]]]]$end<- list(24, 114, 205, 296, 336, 347, 363, 405)
+i=1:6
+AA_atlas[[loci[2]]] <- data.frame(exon=paste("exon", paste(i, i+1, sep=":"), sep="_"), stringsAsFactors = FALSE)
+AA_atlas[[loci[[2]]]]$bound<-list(1, 91, 183, 275, 315, 326)
 
 
-save (AA_atlas, file="AA_atlas.rda")
+i=1:7
+AA_atlas[[loci[[3]]]]<-data.frame(exon=paste("exon", paste(i, i+1, sep=":"), sep="_"), stringsAsFactors = FALSE)
+AA_atlas[[loci[[3]]]]$bound<-list(1, 91, 182, 273, 313, 324, 340)
 
+i=1:5
+AA_atlas[[loci[[4]]]]<-data.frame(exon=paste("exon", paste(i, i+1, sep=":"), sep="_"), stringsAsFactors = FALSE)
+AA_atlas[[loci[[4]]]]$bound<-list(5, 95, 189, 226, 234)
 
+View(AA_atlas)
